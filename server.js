6f73d3fd63587b1b3fcff78a40f3facaf60e3f58
unknown <Semijacks08>
2019-11-06 17:04:07 +0100

    made some changes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var cors = require("cors");
 var db = mongoose.connect("mongodb://localhost/swag-shop");
 
 var Product = require("./model/product");
 var WishList = require("./model/wishlist");
 
 var app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,6 +21,63 @@ app.post("/product", function(request, response) {
       response.status(500).send({ error: "Could not save product" });
     } else {
       response.status(200).send(savedProduct);
+    }
+  });
+});
+
+app.get("/product", function(request, response) {
+  Product.find({}, function(err, products) {
+    if (err) {
+      response.status(500).send({ error: "Could not fetch products" });
+    } else {
+      response.send(products);
+    }
+  });
+});
+
+app.get("/wishlist", function(request, response) {
+  WishList.find({})
+    .populate({ path: "products", model: "Product" })
+    .exec(function(err, wishLists) {
+      if (err) {
+        response.status(500).send({ error: "Could not fetch wishlists" });
+      } else {
+        response.status(200).send(wishLists);
+      }
+    });
+});
+
+app.post("/wishlist", function(request, response) {
+  var wishList = new WishList();
+  wishList.title = request.body.title;
+
+  wishList.save(function(err, newWishList) {
+    if (err) {
+      response.status(500).send({ error: "could not create wishlist" });
+    } else {
+      response.send(newWishList);
+    }
+  });
+});
+
+app.put("/wishlist/product/add", function(request, response) {
+  Product.findOne({ _id: request.body.productId }, function(err, product) {
+    if (err) {
+      response.status(500).send({ error: "Could not add item to wishlist" });
+    } else {
+      WishList.update(
+        { _id: request.body.wishListId },
+        { $addToSet: { products: product._id } },
+        function(err, wishList) {
+          if (err) {
+            response
+              .status(500)
+              .send({ error: "Could not add item to wishlist" });
+          } else {
+            response.send("successfully added to wishlist");
+          }
+        }
+      );
     }
   });
 });
